75beb99719f4193fa7572cceca6084a4ae1a73c5
Yanny01 <yanren2012@gmail.com>
2021-01-27 10:28:50 +1300

    4th line, Yay!

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,5 +4,6 @@ public class PartD {
 		System.out.println("Hi, my name is ____________");
 		System.out.println("My favourite colour is ____________");
 		System.out.println("My favourite mode of transportation is _________");
+		System.out.println("The best day of the week is_______________");
 	}
 }
