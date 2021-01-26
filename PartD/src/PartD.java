b9f3692951c97b99c466282a3a2bc75666d0e509
Yanny01 <yanren2012@gmail.com>
2021-01-27 10:27:57 +1300

    third line

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -3,5 +3,6 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is ____________");
 		System.out.println("My favourite colour is ____________");
+		System.out.println("My favourite mode of transportation is _________");
 	}
 }
